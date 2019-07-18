541adaddc66e93d461ada8708e5c379523890318
JosueDiaz1722 <30628425+JosueDiaz1722@users.noreply.github.com>
2019-07-17 22:11:02 -0500

    Jumbotron

--- proyecto-Web/src/app/inicio/inicio.component.ts
+++ proyecto-Web/src/app/inicio/inicio.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class InicioComponent  {
 
 }
