e3934f123b047f6dedc4097e66030ab8ccf83b13
Jessica_Altamirano <jessy2995@hotmail.com>
2019-07-28 11:18:58 -0500

    perfil y transporte

--- proyecto-Web/src/app/app.module.ts
+++ proyecto-Web/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {CarouselModule} from 'primeng/carousel';
 import { AppRoutingModule } from './app-routing.module';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { ContactanosComponent } from './contactanos/contactanos.component';
@@ -12,6 +13,12 @@ import { PerfilComponent } from './perfil/perfil.component';
 import { TransporteComponent } from './transporte/transporte.component';
 import { PedidosComponent } from './pedidos/pedidos.component';
 import { CompraComponent } from './compra/compra.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+
 
 
 @NgModule({
@@ -29,7 +36,15 @@ import { CompraComponent } from './compra/compra.component';
   imports: [
     BrowserModule,
     NgbModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule ,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- proyecto-Web/src/app/transporte/transporte.component.ts
+++ proyecto-Web/src/app/transporte/transporte.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-transporte',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transporte.component.css']
 })
 export class TransporteComponent implements OnInit {
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
   }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+  
+  cars=[];
+
+  constructor() {
+      this.cars = [
+          {vin: 'r3278r2', year: 2010, brand: 'Audi', color: 'Black', url:'playa.png'},
+          {vin: 'jhto2g2', year: 2015, brand: 'BMW', color: 'White'},
+          {vin: 'h453w54', year: 2012, brand: 'Honda', color: 'Blue'},
+          {vin: 'g43gwwg', year: 1998, brand: 'Renault', color: 'White'},
+          {vin: 'gf45wg5', year: 2011, brand: 'VW', color: 'Red'},
+          {vin: 'bhv5y5w', year: 2015, brand: 'Jaguar', color: 'Blue'},
+          {vin: 'ybw5fsd', year: 2012, brand: 'Ford', color: 'Yellow'},
+          {vin: '45665e5', year: 2011, brand: 'Mercedes', color: 'Brown'},
+          {vin: 'he6sb5v', year: 2015, brand: 'Ford', color: 'Black'}
+      ];
+  }
+
+ 
+
 
 }
