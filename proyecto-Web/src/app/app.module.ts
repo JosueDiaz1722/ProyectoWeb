bccaa16b1185b20eaae137ff39ad1fdeb614c8e3
JosueDiaz1722 <30628425+JosueDiaz1722@users.noreply.github.com>
2019-07-27 21:49:08 -0500

    iconos

--- proyecto-Web/src/app/app.module.ts
+++ proyecto-Web/src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { ContactanosComponent } from './contactanos/contactanos.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PaquetesComponent } from './paquetes/paquetes.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { TransporteComponent } from './transporte/transporte.component';
 import { PedidosComponent } from './pedidos/pedidos.component';
 import { CompraComponent } from './compra/compra.component';
+import { FooterComponent } from './footer/footer.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { NgImageSliderModule } from 'ng-image-slider';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 
 @NgModule({
@@ -24,13 +30,20 @@ import { CompraComponent } from './compra/compra.component';
     PerfilComponent,
     TransporteComponent,
     PedidosComponent,
-    CompraComponent
+    CompraComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule,
+    NgImageSliderModule,
+    AngularFontAwesomeModule
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- proyecto-Web/src/app/inicio/inicio.component.ts
+++ proyecto-Web/src/app/inicio/inicio.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @Component({
@@ -7,6 +7,72 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent  {
-
+export class InicioComponent implements OnInit  {
+  /*cards = [
+    {
+      title: 'Card Title 1',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 2',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 3',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 4',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 5',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 6',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 7',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 8',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+    {
+      title: 'Card Title 9',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg'
+    },
+  ];
+  slides: any = [[]];
+  chunk(arr, chunkSize) {
+    let R = [];
+    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
+      R.push(arr.slice(i, i + chunkSize));
+    }
+    return R;
+  }*/
+  ngOnInit() {
+    //this.slides = this.chunk(this.cards, 3);
+  }
 }
