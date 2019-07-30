5b605f72a0d1ef54326641b75800a252cf0c41b3
JosueDiaz1722 <30628425+JosueDiaz1722@users.noreply.github.com>
2019-07-30 14:45:21 -0500

    firebase

--- proyecto-Web/src/environments/environment.prod.ts
+++ proyecto-Web/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyA9ivu98QIcUmvHI7mkSdTO-lcH2vAy3h0",
+    authDomain: "proyectoweb-f73c6.firebaseapp.com",
+    databaseURL: "https://proyectoweb-f73c6.firebaseio.com",
+    projectId: "proyectoweb-f73c6",
+    storageBucket: "",
+    messagingSenderId: "732342767389",
+    appId: "1:732342767389:web:28b0f779851a9464"
+  }
 };
--- proyecto-Web/src/environments/environment.ts
+++ proyecto-Web/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyA9ivu98QIcUmvHI7mkSdTO-lcH2vAy3h0",
+    authDomain: "proyectoweb-f73c6.firebaseapp.com",
+    databaseURL: "https://proyectoweb-f73c6.firebaseio.com",
+    projectId: "proyectoweb-f73c6",
+    storageBucket: "",
+    messagingSenderId: "732342767389",
+    appId: "1:732342767389:web:28b0f779851a9464"
+  }
 };
 
 /*
